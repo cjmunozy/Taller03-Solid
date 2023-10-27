e25739f71786a631d599331d23047cc1d583476b
StevenLino <stemlino@espol.edu.ec>
2023-10-26 21:47:02 -0500

    codigo por defecto

---
+++ src/main/java/pacman_projects/no_es_un_proyecto/Compra.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacman_projects.no_es_un_proyecto;
+import java.util.List;
+/**
+ *
+ * @author USUARIO
+ */
+public class Compra {
+    private Pago pago;
+    private PagoPaypal pagopaypal;
+    private List articulos;
+    public Compra(Pago pago){
+        
+    }
+    public Compra(PagoPaypal pagopaypal){
+        
+    }
+    public void agrgarArticulo(Articulo articulo){
+        
+    }
+    public void removerArticulo(Articulo articulo){
+        
+    }
+}
---
+++ src/main/java/pacman_projects/no_es_un_proyecto/Pago.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacman_projects.no_es_un_proyecto;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Pago {
+    public void realizarCobro(double monto){
+        //carga el monto de compra al medio de pago
+    }
+    
+    public void calcularImpuestoFactura(){
+        //calcula los impuestos aplicados a la compra
+    }
+    
+    public void generarFactura(){
+        //genera una nueva factura
+    }
+}
